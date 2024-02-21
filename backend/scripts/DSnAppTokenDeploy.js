3446988c8a5e5d00a9f026192b1e8a1664962764
LukaszG92 <lukasz.gajewski15@gmail.com>
2024-02-21 19:16:58 +0100

    Updated DSnAppTokenDeploy.js

--- backend/scripts/DSnAppTokenDeploy.js
+++ backend/scripts/DSnAppTokenDeploy.js
@@ -3,12 +3,14 @@ const {Web3} = require('web3')
 require('dotenv').config()
 
 let ganachePort = process.env.GANACHE_PORT || 8545
-web3 = new Web3(`http://localhost:${ganachePort}`)
+let web3 = new Web3(`http://localhost:${ganachePort}`)
 
-bytecode = fs.readFileSync(process.env.BIN_PATH).toString()
-abi = JSON.parse(fs.readFileSync(process.env.ABI_PATH).toString())
+let bytecode = fs.readFileSync(process.env.BIN_PATH).toString()
+let abi = JSON.parse(fs.readFileSync(process.env.ABI_PATH).toString())
+
 
 let DSnAppToken = new web3.eth.Contract(abi)
+
 DSnAppToken.deploy({data:bytecode})
     .send({
         from: process.env.GANACHE_ADDRESS,
