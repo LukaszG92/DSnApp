1372ed0da826296f107afc72d1536ed6180ad609
LukaszG92 <lukasz.gajewski15@gmail.com>
2024-03-08 11:00:44 +0100

    Contract update

--- backend/src/contracts/DSnAppToken.sol
+++ backend/src/contracts/DSnAppToken.sol
@@ -5,21 +5,41 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DSnAppToken is ERC20{
     address private _owner;
+    bool private  _isStopped = false;
 
     constructor() ERC20("DSnAppToken", "DST"){
         _owner = msg.sender;
     }
 
     modifier onlyOwner(){
-        require(isOwner(), "You have not the permission to use this function");
+        require(isOwner(), "You have not the permission to use this function.");
         _;
+    }
+
+    modifier stoppedInEmergency {
+        require(!isStopped, "Contract under emergency stop.");
+        _;
+    }
+
+    function stopContract() public onlyOwner {
+        isStopped = true;
+    }
+
+    function resumeContract() public onlyOwner {
+        isStopped = false;
     }
 
     function isOwner() private view returns (bool){
         return msg.sender==_owner;
     }
 
-    function mintToken(address receiver, uint256 amount) public onlyOwner{
+    function mintToken(address receiver, uint256 amount) public stoppedInEmergency onlyOwner{
+        require(receiver != address(0), "Cannot mint tokens to a NULL address.");
+        require(amount > 0, "Cannot mint 0 tokens.");
+
+        uint256 receiverBalance = balanceOf(receiver);
+
         _mint(receiver, amount*10**18);
+        assert(balanceOf(receiver) == receiverBalance + amount*10**18);
     }
 }
